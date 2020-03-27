ca7044a0792b5566262923da9b0f0647765f5190
saadkoker <60635704+saadkoker@users.noreply.github.com>
2020-03-27 01:39:39 -0400

    Finally got a piece on the board

--- ChessBoardFiles/ChessScreen.java
+++ ChessBoardFiles/ChessScreen.java
@@ -8,9 +8,9 @@ import java.awt.image.BufferedImage;
 
 public class ChessScreen{
 
-private static ChessBoard cb = new ChessBoard(); //our ChessBoard object
-private static ClickListener click = new ClickListener(); //our ClickListener object
-private static Convert conv = new Convert(); //our Convert object
+	private static ChessBoard cb = new ChessBoard(); //our ChessBoard object
+	private static ClickListener click = new ClickListener(); //our ClickListener object
+	private static Convert conv = new Convert(); //our Convert object
 
 
     public void startScreen(int h, int l) throws InterruptedException{ //this method is called by another class and builds the chess board -> this will be the method that calls the Game class
@@ -35,38 +35,19 @@ private static Convert conv = new Convert(); //our Convert object
 
         board.addMouseListener(click);
         board.setSize(h,l);
-		
-		/*
-        JLabel whiteRook = new JLabel(new ImageIcon("pieces/Rook2.png"));
-        cb.setLayout(null);
-    	Dimension size = whiteRook.getPreferredSize();
-    	whiteRook.setBounds(75, 75, size.width, size.height);
-    	cb.add(whiteRook);
-		*/
 
         board.setVisible(true);
         board.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         board.setResizable(true);
-
-        try{
-            BufferedImage img =  ImageIO.read(new File("queen1.png"));
-            System.out.println("shit");
-            ImageIcon icon = new ImageIcon(img);
-            JLabel label = new JLabel(icon);
-            board.add(label);
-
-        }catch(Exception E){
-            System.out.println("shit");
-        }
-
+        
         boolean clickyTime = true; 
         int count = 0;
-        System.out.println("You have 25 clicks, dont waste them!");
+        System.out.println("You have 25 clicks, dont waste them! hehe");
 
         while(clickyTime){ //just a test loop
 
             int[] clickCoords = click.getClick(); //getting a click -> note: we must get our clicks from this method
-            clickCoords = conv.convertArr(clickCoords, 22);
+            clickCoords = conv.convertArr(clickCoords, 60);
             System.out.println(Arrays.toString(clickCoords));
             message.setText("Yikes, bad move.");
             count++;
