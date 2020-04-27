3d41017bbbcb47c61e77c10358fee52b906ef2ab
saadkoker <60635704+saadkoker@users.noreply.github.com>
2020-04-27 03:46:57 -0400

    Cont. of space removing

--- ChessBoardFiles/Coordinate.java
+++ ChessBoardFiles/Coordinate.java
@@ -1,21 +1,21 @@
 public class Coordinate
 {
-  private int x;
-  private int y;
-  
-  public Coordinate(int x_val, int y_val)
-  {
-    x = x_val;
-    y = y_val;
-  }
-  
-  public int getX()
-  {
-    return x;
-  }
-    
-  public int getY()
-  {
-    return y; //returns y
-  }
+	private int x;
+	private int y;
+	
+	public Coordinate(int x_val, int y_val)
+	{
+		x = x_val;
+		y = y_val;
+	}
+	
+	public int getX()
+	{
+		return x;
+	}
+		
+	public int getY()
+	{
+		return y; //returns y
+	}
 } 
